c4c4139cf01119753172b0ad4444d7b880348ca5
li <125846662@qq.com>
2026-01-23 10:42:02 +0800

    feat:logout按钮位置调整

--- frontend/components/layout/leftPanel/index.tsx
+++ frontend/components/layout/leftPanel/index.tsx
@@ -78,8 +78,10 @@ const LeftPanel: React.FC<IProps> = (props) => {
       {/* Header */}
       <div className="p-4 border-b border-gray-200">
         <div className="flex items-center justify-between mb-4">
-          <User />
-          <h2 className="text-lg font-semibold text-gray-800">Pixelle-Studio</h2>
+          <div className="flex flex-row gap-2 items-center">
+            <User />
+            <h2 className="text-lg font-semibold text-gray-800">Pixelle-Studio</h2>
+          </div>
           <CollaspeButton 
             isCollapsed={isCollapsed}
             onClick={handleCollapse}
