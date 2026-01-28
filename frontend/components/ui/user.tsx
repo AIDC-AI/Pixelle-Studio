417d31595e4d3a18537844b3bf48a21f839c8bf8
li <125846662@qq.com>
2026-01-28 17:19:31 +0800

    feat：消息列表顶部显示个人信息和logout

--- frontend/components/ui/user.tsx
+++ frontend/components/ui/user.tsx
@@ -1,51 +1,43 @@
 import { useApp } from "@/context"
-import { Avatar, DropdownMenu } from "radix-ui"
+import { UserRound } from "lucide-react"
+import { Avatar, HoverCard } from "radix-ui"
 
 const User = () => {
-    const { user, logout } = useApp()
+    const { user } = useApp()
 
     return (
-        <DropdownMenu.Root>
-			<DropdownMenu.Trigger asChild>
-				<Avatar.Root className="inline-flex items-center justify-center align-middle overflow-hidden select-none w-7 h-7 rounded-full bg-gray-100">
-                    <Avatar.Fallback className="text-gray-900 text-xs">{user?.username?.[0]}</Avatar.Fallback>
+        <HoverCard.Root>
+			<HoverCard.Trigger asChild>
+				<Avatar.Root className="flex items-center justify-center w-6 h-6 rounded-full bg-[#0078d4] p-1">
+                    {/* <Avatar.Fallback className="text-gray-900 text-xs">{user?.username?.[0]}</Avatar.Fallback> */}
+                    <UserRound className="text-white" />
                 </Avatar.Root>
-			</DropdownMenu.Trigger>
+			</HoverCard.Trigger>
 
-			<DropdownMenu.Portal>
-				<DropdownMenu.Content 
-                    className="w-40 bg-white border border-gray-300 rounded-md p-2 shadow-[0px_10px_38px_-10px_rgba(22,23,24,0.35),0px_10px_20px_-15px_rgba(22,23,24,0.2)] animate-up-fade will-change-[transform,opacity]"
+			<HoverCard.Portal>
+				<HoverCard.Content 
+                    className="flex flex-col gap-1 w-40 bg-gray-900 rounded-md p-2 animate-up-fade will-change-[transform,opacity]"
                     sideOffset={5}
                 >
                     <span
-                        className="text-xs text-gray-900 truncate cursor-default"
+                        className="text-sm text-white truncate cursor-default"
                     >
-						{`用户名：${user?.username}`}
+						{user?.username}
 					</span>
 					<span
-                        className="text-xs text-gray-900 truncate cursor-default"
+                        className="text-sm text-white truncate cursor-default"
                     >
-						{`邮箱：${user?.email}`}
+						{user?.email}
 					</span>
-                    <button
+                    {/* <button
                         className="border border-gray-400 hover:border-gray-800 hover:bg-gray-100 text-xs text-gray-900 w-full rounded-sm px-2"
                         onClick={logout}
                     >
                         登出
-                    </button>
-					<DropdownMenu.Arrow              
-                        className="fill-gray-300 translate-x-0.5" 
-                        width={14} 
-                        height={6}  
-                    />
-                    <DropdownMenu.Arrow              
-                        className="fill-white" 
-                        width={10} 
-                        height={5}  
-                    />
-				</DropdownMenu.Content>
-			</DropdownMenu.Portal>
-		</DropdownMenu.Root>
+                    </button> */}
+				</HoverCard.Content>
+			</HoverCard.Portal>
+		</HoverCard.Root>
     )
 }
 
