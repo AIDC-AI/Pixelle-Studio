8415d5bb0df2675d8c21df5df46380266d04f0b4
li <125846662@qq.com>
2026-02-02 11:11:20 +0800

    fea：弹出框滚动修改

--- frontend/components/ui/toolConfigureModal.tsx
+++ frontend/components/ui/toolConfigureModal.tsx
@@ -113,7 +113,7 @@ const ToolConfigureModal: React.FC<IProps> = (props) => {
                         const data = Object.fromEntries(formData)
                         await handleSubmit(data)
                     }}
-                    className="flex flex-col flex-1 gap-4 mt-4 overflow-auto"
+                    className="flex flex-col flex-1 gap-4 mt-4 overflow-auto overscroll-contain"
                 >
                     <FormInput 
                         name="name"
